de0206f3c16a47888c7fc549d69122c6855244ac
determin1st <baklanoid@gmail.com>
2019-09-11 02:30:09 +0500

    rev

--- test-2/index.js
+++ test-2/index.js
@@ -1,29 +1,40 @@
 "use strict";
 window.addEventListener('load', function() {
     ////
-    var test = function()
+    // prepare
+    var token = {};
+    var url  = 'https://accounts.google.com/o/oauth2/v2/auth';
+    var data = {
+        client_id: '167739649429-tq1c78uvikndmf8sv5c0290rtnrpq771.apps.googleusercontent.com',
+        response_type: 'token',
+        redirect_uri: 'https://raw.githack.com/determin1st/httpFetch/master/test-2/index.html',
+        scope: 'profile'
+    };
+    var a, b, c;
+    ////
+    // check token set
+    if (a = window.location.hash)
     {
-        var url  = 'https://accounts.google.com/o/oauth2/v2/auth';
-        var data = {
-            client_id: '167739649429-tq1c78uvikndmf8sv5c0290rtnrpq771.apps.googleusercontent.com',
-            response_type: 'token',
-            redirect_uri: 'https://raw.githack.com/determin1st/httpFetch/master/test-2/index.html',
-            scope: 'profile'
-        };
-        /***
-        httpFetch.post(url, data, function(ok, res) {
-            debugger;
+        // parse token
+        b = a.substring(1).split('&');
+        b.forEach(function(a) {
+            a = a.split('=');
+            token[a[0]] = a[1];
         });
-        /***/
+        debugger;
+        // clear location and show document body
+        window.location.hash = '';
+        document.body.style.display = '';
+        // prepare test
+        document.querySelector('button').addEventListener('click', function(e) {
+        });
+    }
+    else
+    {
+        // redirect
         window.location = url+'?client_id='+data.client_id+
             '&response_type='+data.response_type+
             '&redirect_uri='+data.redirect_uri+
             '&scope='+data.scope;
     }
-    document.querySelector('button').addEventListener('click', function(e) {
-        test();
-    });
-    document.body.style.display = '';
-    console.log(window.location);
-    ////
 });
