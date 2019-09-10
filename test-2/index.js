56c13ec71dde2c1777a7d15131018b6cc9c96b1d
determin1st <baklanoid@gmail.com>
2019-09-11 02:11:56 +0500

    rev

--- test-2/index.js
+++ test-2/index.js
@@ -5,15 +5,25 @@ window.addEventListener('load', function() {
     {
         var url  = 'https://accounts.google.com/o/oauth2/v2/auth';
         var data = {
-            client_id: '167739649429-kek60h1r979qdtpa1do04qjnctihnm22.apps.googleusercontent.com',
+            client_id: '167739649429-tq1c78uvikndmf8sv5c0290rtnrpq771.apps.googleusercontent.com',
+            response_type: 'token',
             redirect_uri: 'https://raw.githack.com/determin1st/httpFetch/master/test-2/index.html',
             scope: 'profile'
         };
+        /***
         httpFetch.post(url, data, function(ok, res) {
             debugger;
         });
+        /***/
+        window.location = url+'?client_id='+data.client_id+
+            '&response_type='+data.response_type+
+            '&redirect_uri='+data.redirect_uri+
+            '&scope='+data.scope;
     }
-    test();
+    document.querySelector('button').addEventListener('click', function(e) {
+        test();
+    });
     document.body.style.display = '';
+    console.log(window.location);
     ////
 });
