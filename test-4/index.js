e154dd4298963e4ee517d79f8843f05ba1a18625
determin1st <baklanoid@gmail.com>
2019-10-20 02:58:20 +0500

    rev

--- test-4/index.js
+++ test-4/index.js
@@ -2,24 +2,31 @@
 window.addEventListener('load', function() {
     ////
     // prepare
-    var token = {}, a, b;
+    var a, b;
     var b1  = document.querySelector('button.b1');
     var b2  = document.querySelector('button.b2');
+    var b3  = document.querySelector('button.b2');
     var h1  = document.querySelector('h1');
-    var img = document.querySelector('img');
-    // check
+    var data = {
+        client_id: 'Iv1.0083b2083e6bf04f',
+        client_secret: 'b05178a2a62f11cc6e5603665e356b7bd5e68ad7',
+        redirect_uri: 'https://raw.githack.com/determin1st/httpFetch/master/test-4/index.html',
+        code: '',
+        token: ''
+    };
+    // check initial state
     debugger;
-    if (a = window.location.hash)
+    if (a = window.location.search && a.indexOf('?code=') === 0)
     {
-        // parse token
+        // get temporary code
         b = a.substring(1).split('&');
         b.forEach(function(a) {
             a = a.split('=');
-            token[a[0]] = a[1];
+            data[a[0]] = a[1];
         });
         // clear location
-        window.location.hash = '';
-        // done
+        window.location.search = '';
+        // show next step
         setTimeout(function() {
             h1.innerText = '';
             b2.classList.remove('hidden');
@@ -27,7 +34,7 @@ window.addEventListener('load', function() {
     }
     else
     {
-        // done
+        // show first step
         setTimeout(function() {
             h1.innerText = '';
             b1.classList.remove('hidden');
@@ -36,46 +43,43 @@ window.addEventListener('load', function() {
     // set handlers
     b1.addEventListener('click', function(e) {
         ////
-        // auth redirect
+        // step1: auth redirect
         var url  = 'https://github.com/login/oauth/authorize';
-        var data = {
-            client_id: 'Iv1.0083b2083e6bf04f',
-            redirect_uri: 'https://raw.githack.com/determin1st/httpFetch/master/test-4/index.html'
-        };
-        window.location = url+'?client_id='+data.client_id+
-            '&redirect_uri='+data.redirect_uri;
+        window.location = url+'?client_id='+data.client_id+'&redirect_uri='+data.redirect_uri;
     });
     b2.addEventListener('click', function(e) {
         ////
+        // step2: exchange
         // prepare
-        img.classList.remove('exist');
-        img.src = '';
-        h1.innerText = 'fetching..';
+        h1.innerText = 'exchanging code for token..';
         b2.disabled = true;
-        ////
         // send request
         httpFetch({
-            //url: 'https://www.googleapis.com/drive/v3/files',
-            url: 'https://www.googleapis.com/oauth2/v1/userinfo',
-            headers: {
-                Authorization: 'Bearer '+token.access_token
+            url: 'https://github.com/login/oauth/access_token',
+            data: {
+                client_id: data.client_id,
+                client_secret: data.client_secret,
+                code: data.code
             }
         }, function(ok, res) {
             if (ok)
             {
+                debugger;
+                /***
                 h1.innerText = res.name;
                 if (res.picture)
                 {
                     img.src = res.picture;
                     img.classList.add('exist');
                 }
+                /***/
             }
             else
             {
                 h1.innerText = 'error';
                 console.log(res);
             }
-            b2.disabled = false;
+            //b2.disabled = false;
         });
     });
 });
